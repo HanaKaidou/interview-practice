ccaebb3d453803d160b1222ec2e62022f3c0150e
HanaKaidou <58365725+HanaKaidou@users.noreply.github.com>
2024-03-09 16:39:02 +0800

    init

---
+++ redis-practice/src/main/java/com/csh/interview/redispractice/RedisPracticeApplication.java
@@ -0,0 +1,14 @@
+package com.csh.interview.redispractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisPracticeApplication.class, args);
+        System.out.println("启动成功");
+    }
+
+}
