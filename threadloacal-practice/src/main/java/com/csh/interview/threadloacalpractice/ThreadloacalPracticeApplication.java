71ff69d6c7d5d010b33f8ab1ef4a03cccbc8dca2
HanaKaidou <58365725+HanaKaidou@users.noreply.github.com>
2024-05-30 23:30:21 +0800

    init

--- threadloacal-practice/src/main/java/com/csh/interview/threadloacalpractice/ThreadloacalPracticeApplication.java
+++ threadloacal-practice/src/main/java/com/csh/interview/threadloacalpractice/ThreadloacalPracticeApplication.java
@@ -8,7 +8,6 @@ public class ThreadloacalPracticeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ThreadloacalPracticeApplication.class, args);
-
     }
 
 }
